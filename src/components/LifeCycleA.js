c18c48a115fc516f24921ea3214f725519bc4898
Piriyalini Niththiyanantham <priyalini@unicomtechnology.co.uk>
2023-03-10 08:59:49 +0530

    Component updating lifecycle methods

--- src/components/LifeCycleA.js
+++ src/components/LifeCycleA.js
@@ -1,35 +1,96 @@
 import React, { Component } from 'react'
 import LifeCycleB from './LifeCycleB'
 
-// Mounting Lifecycle methods
 class LifeCycleA extends Component {
-    constructor(props) {
-      super(props)
+//     // Mounting Lifecycle methods
+
+//     //Mounting 1
+//     constructor(props) {
+//       super(props)
     
-      this.state = {
-         name: 'Priya'
-      }
-      console.log('LifecyclemethodA Constructor')
-    }
+//       this.state = {
+//          name: 'Priya'
+//       }
+//       console.log('LifecyclemethodA Constructor')
+//     }
 
-    static getDerivedStateFromProps(props, state){
-        console.log('LifecyclemethodA getDerivedStateFromProps')
-        return null
-    }
+//     //Mounting 2
+//     static getDerivedStateFromProps(props, state){
+//         console.log('LifecyclemethodA getDerivedStateFromProps')
+//         return null
+//     }
 
-    componentDidMount(){
-        console.log('LifecyclemethodA componentDidMount')
-    }
+//     //Mounting 4
+//     componentDidMount(){
+//         console.log('LifecyclemethodA componentDidMount')
+//     }
 
-  render() {
-    console.log('LifecyclemethodA render')
-    return (
-        <div>
-            <div>LifeCycle A</div>
-            <LifeCycleB/>
-        </div>
-    )
+//     //Mounting 3
+//   render() {
+//     console.log('LifecyclemethodA render')
+//     return (
+//         <div>
+//             <div>LifeCycle A</div>
+//             <LifeCycleB/>
+//         </div>
+//     )
+//   }
+
+
+// Updating Lifecycle methods
+constructor(props) {
+    super(props)
+  
+    this.state = {
+       name: 'Priya'
+    }
+    console.log('LifecyclemethodA Constructor')
   }
+
+  //     //Update 1
+  static getDerivedStateFromProps(props, state){
+      console.log('LifecyclemethodA getDerivedStateFromProps')
+      return null
+  }
+
+  componentDidMount(){
+      console.log('LifecyclemethodA componentDidMount')
+  }
+
+  //     //Update 2
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('LifecyclemethodA shouldComponentUpdate')
+    return true
+  }
+
+  //     //Update 4
+  getSnapshotBeforeUpdate(prevProps,prevState){
+    console.log('LifecyclemethodA getSnapshotBeforeUpdate')
+    return null
+  }
+
+  //     //Update 5
+  componentDidUpdate(prevProps, prevState, snapshot){
+    console.log('LifecyclemethodA componentDidUpdate')
+  }
+
+  changeState = () => {
+    this.setState({
+        name: 'Kishan'
+    })
+  }
+
+  //     //Update 3
+render() {
+  console.log('LifecyclemethodA render')
+  return (
+      <div>
+          <div>LifeCycle A</div>
+          <button onClick={this.changeState}>Change state</button>
+          <LifeCycleB/>
+      </div>
+  )
+}
 }
 
 export default LifeCycleA
--- src/components/LifeCycleB.js
+++ src/components/LifeCycleB.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react'
 
 class LifeCycleB extends Component {
+    // // Mounting Lifecycle methods
+    // constructor(props) {
+    //     super(props)
+      
+    //     this.state = {
+    //        name: 'Priya'
+    //     }
+    //     console.log('LifecyclemethodB Constructor')
+    //   }
+  
+    //   static getDerivedStateFromProps(props, state){
+    //       console.log('LifecyclemethodB getDerivedStateFromProps')
+    //       return null
+    //   }
+  
+    //   componentDidMount(){
+    //       console.log('LifecyclemethodB componentDidMount')
+    //   }
+  
+    // render() {
+    //   console.log('LifecyclemethodB render')
+    //   return (
+    //     <div>LifeCycle B</div>
+    //   )
+    // }
+
+
+    // Updating Lifecycle methods
     constructor(props) {
         super(props)
       
@@ -17,6 +45,20 @@ class LifeCycleB extends Component {
   
       componentDidMount(){
           console.log('LifecyclemethodB componentDidMount')
+      }
+
+      shouldComponentUpdate(nextProps, nextState){
+        console.log('LifecyclemethodB shouldComponentUpdate')
+        return true
+      }
+    
+      getSnapshotBeforeUpdate(prevProps,prevState){
+        console.log('LifecyclemethodB getSnapshotBeforeUpdate')
+        return null
+      }
+    
+      componentDidUpdate(prevProps, prevState, snapshot){
+        console.log('LifecyclemethodB componentDidUpdate')
       }
   
     render() {
